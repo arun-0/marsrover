b416d6edf0e52ef25259f766348ed32d725ee69d
Arun Parihar <arun.parihar@bp.com>
2020-08-24 13:09:13 +0100

    tests for motion anywhere other than from Origin

--- src/test/java/com/lme/robot/mars/MarsRobotTest.java
+++ src/test/java/com/lme/robot/mars/MarsRobotTest.java
@@ -33,4 +33,37 @@ public class MarsRobotTest {
         Assertions.assertEquals("0 -1 S", robot.tellCoordinates());
     }
 
+    @Test
+    public void roverShouldMoveForwardFromOriginCorrectly() {
+        Coordinates coor = new Coordinates();
+
+        // create an instance of an anonymous class of Rover abstract class, in order to test its move() method
+        MarsRobot robot = MarsRobot.init("MarsRobot-A", coor);
+
+        //turn Left and move
+        robot.move(1);
+
+        Assertions.assertEquals("1 0 E", robot.tellCoordinates());
+    }
+
+    @Test
+    public void roverShouldMoveAndTurnCorrectly() {
+        Coordinates coor = new Coordinates(3, 4, 90);
+
+        MarsRobot robot = MarsRobot.init("MarsRobot-A", coor);
+        robot.move(1);
+        Assertions.assertEquals("3 5 N", robot.tellCoordinates());
+
+        // turn right and move
+        robot.turn(-90);
+        robot.move(1);
+        Assertions.assertEquals("4 5 E", robot.tellCoordinates());
+
+        // turn left and move
+        robot.turn(90);
+        robot.move(1);
+        Assertions.assertEquals("4 6 N", robot.tellCoordinates());
+
+    }
+
 }
